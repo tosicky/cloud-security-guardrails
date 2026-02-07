7972377959b4940ffd3e7d3543aa7d6c7960a44e
Oadele <oadele>
2026-02-07 08:55:59 -0400

    update iam permissions

--- infra/live/account-baseline/providers.tf
+++ infra/live/account-baseline/providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = var.region
-  profile = "iamadmin-general"
+  # profile = "iamadmin-general"
   # assume_role {
   #   role_arn = "arn:aws:iam::061451663077:role/github-org-bootstrap"
   # }
